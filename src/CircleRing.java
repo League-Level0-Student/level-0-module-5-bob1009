1e263a524a78df0db427b7ccb32775f7c8a1d7b1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-19 17:49:29 -0700

    finshed circlering and started code for life

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,27 +9,36 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
     	Robot jeff =new Robot();
         // 1. Make a new Robot
-jeff.moveTo(150,200);
+
 
        //  2. Set your robot’s position to x=150 and y=200
+jeff.moveTo(150,200);
 
         // 3. Put the robot's pen down
-
+	jeff.penDown();
         // 4. Set the robot’s speed to 10
-
+	jeff.setSpeed(10000000);
+	
         // 5. Do everything below here 360 times (use i as the counter)
-
+	for (int i = 0; i <360 ; i++) {
+		
 	        // 6. Move the robot 3 pixels
-
+		jeff.move(3);
        //  7. Turn the robot 1 degree
-
+		jeff.turn(1);
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
+			if(i%20==0) {
+				for	(int j = 0; j <360 ; j++) {
+					jeff.move(1);
+				jeff.turn(1);
+				}
+			}
     			
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
 				// 10. Move the robot 1 pixel
 
 				// 11. Turn the robot 1 degree
+	}
 }
 }
--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,20 +7,27 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+import org.omg.CORBA.INTERNAL;
 
 
 public class Code4Life {
 	/*
      * Ask the user how many hours they spent coding this week.
-     * 
+      
+    
+    
+  
      * 1. If it's 3 or more, tell them they're a Code Ninja.
      * 
      * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
      * 
      * 3. If it's more than 5, play the Batman theme song.
      */
-
-
+	  String jeff = JOptionPane.showInputDialog("how many hours today have you typed this week?");
+	  int ansewer=Integer.parseInt(jeff);
+	  
     private static void playBatmanTheme() {
     	try {
    		 Clip clip = AudioSystem.getClip();
