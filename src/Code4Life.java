da10c586ac73bb8162c3215ab27cfbba8a6f5f63
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-26 17:49:53 -0700

    working on evilpolygonschallenge and fnished code4life;)

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -11,33 +12,47 @@ import javax.swing.JOptionPane;
 
 import org.omg.CORBA.INTERNAL;
 
-
 public class Code4Life {
 	/*
-     * Ask the user how many hours they spent coding this week.
-      
-    
-    
-  
-     * 1. If it's 3 or more, tell them they're a Code Ninja.
-     * 
-     * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
-     * 
-     * 3. If it's more than 5, play the Batman theme song.
-     */
-	  String jeff = JOptionPane.showInputDialog("how many hours today have you typed this week?");
-	  int ansewer=Integer.parseInt(jeff);
-	  
-    private static void playBatmanTheme() {
-    	try {
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(AudioSystem.getAudioInputStream(Code4Life.class.getResource("batman.wav")));
-   		 clip.start();
-   		 Thread.sleep(60002);
-   	} catch (Exception ex) {
-     	ex.printStackTrace();
-   	}
-    }
+	 * Ask the user how many hours they spent coding this week.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 1. If it's 3 or more, tell them they're a Code Ninja.
+	 * 
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code
+	 * instead.
+	 * 
+	 * 3. If it's more than 5, play the Batman theme song.
+	 */
+	public static void main(String[] args) {
+		String jeff = JOptionPane.showInputDialog("how many hours today have you typed this week?");
+		int ansewer = Integer.parseInt(jeff);
+		if (ansewer > (2)) {
+			JOptionPane.showMessageDialog(null, "you are a CODE NINJA");
 
+		}
+
+		if (ansewer <= (2)) {
+			JOptionPane.showMessageDialog(null, "stop watching yotube and start typing!!!!!!!!");
+		}
+
+		if (ansewer > (5)) {
+			playBatmanTheme();
+		}
+
+	}
+
+	private static void playBatmanTheme() {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Code4Life.class.getResource("batman.wav")));
+			clip.start();
+			Thread.sleep(60002);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -8,20 +8,42 @@ public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
 		//1. Create a robot
-
+		Robot jeff =new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		jeff.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "choose a color please", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"red","green", "blue"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
+		if(colorChoice==0) {
+			jeff.setPenColor(Color.RED);
+			
+		}
 		
+		if(colorChoice==1) {
+			jeff.setPenColor(Color.GREEN);
+		}
+		if(colorChoice==2) {
+			jeff.setPenColor(Color.BLUE);
+		}
+		jeff.penDown();
 		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
+	String question =	JOptionPane.showInputDialog("how many polygon do you want to make");
+	int ansewer= Integer.parseInt(question);
+	//5. Use the robot to draw the number of polygons the user requested.
+	for (int i = 0; i < ansewer; i++) {
+		jeff.move(90);
+		jeff.turn(135);
+		jeff.move(90);
+		jeff.turn(135);
+		jeff.move(90);
+	}
+	
+	
+			
+		}
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
 	}
-}
+
 
